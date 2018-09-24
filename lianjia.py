54708569bbac34d9620cebe06c6c4ff6a1fa9068
Feily Zhang <fei472625@foxmail.com>
2018-09-24 22:56:02 +0800

    Update lianjia.py

--- lianjia.py
+++ lianjia.py
@@ -94,3 +94,74 @@ def getLianjiaData(keys, districts, pageNumber, saveFileRootPath):
                 eleIndex = eleIndex + 1
             eleIndex = 0;
         districtsIndex = districtsIndex + 1;
+
+
+'''
+**********************Function Information******************************
+*
+* Function Name : getLianjiaDataZufang
+* Create Date   : 2018-09-24
+* Discription   : Preliminary access to housing price data(renting a house).
+* Parameter     : districts : All districts and counties of Xi'an, Shaanxi.
+                  pageNumber : There are several pages of JSON data in each district.
+                  saveFileRootPath : Save the root of the file.
+* Return Value  : None
+*
+************************Revision History********************************
+*
+* No   Version   Date        Revised by     Discription
+* 0    V1.0.1    2018-09-24  Feily Zhang    Create this function.
+*
+************************************************************************
+'''
+def getLianjiaDataZufang(districts, pageNumber, saveFileRootPath):
+    num = 0
+    result = ""
+    region = []
+    zone = []
+    meters = []
+    other = []
+    tag = []
+    price = []
+    updateTime = []
+    seeNum = []
+    iniTag = []
+    finalTag = []
+    # The cycle traverses all districts and counties.
+    for dis in range(0, len(districts)):
+        # The loop control per page, then get per districts datas.
+        for index in range(1, pageNumber[dis] + 1):
+            url = "https://xa.lianjia.com/zufang/" + districts[dis] + "/pg" + str(index)
+            print(url)
+            htmlContent = requests.get(url).text
+            soup = BeautifulSoup(htmlContent)
+            region = soup.find_all("span", class_="region")
+            zone = soup.find_all("span", class_="zone")
+            meters = soup.find_all("span", class_="meters")
+            other = soup.find_all("div", class_="con")
+            tag = soup.find_all("div", class_="view-label left")
+            price = soup.find_all("span", class_="num")
+            updateTime = soup.find_all("div", class_="price-pre")
+            seeNum = soup.find_all("div", class_="square")
+            for per in range(0, len(region)):
+                iniTag = tag[per].find_all("span")
+                for perTag in range(0, len(iniTag)):
+                    finalTag.insert(perTag, iniTag[perTag].string)
+                result = result + str(per + num) + ",,," + region[per].string + ",,," + zone[per].string + ",,,"
+                result = result + meters[per].string + ",,," + other[per].find("a").string + ",,," + str(finalTag) + ",,,"
+                result = result + price[per].string + ",,," + updateTime[per].string + ",,," + seeNum[per].find("span").string + "\n"
+                iniTag.clear()
+                finalTag.clear()
+            writeFile(saveFileRootPath + '/lianjia_original_zufang_' + districts[dis] + ".txt", result)
+            print(result)
+            num = num + len(region)
+            region.clear()
+            zone.clear()
+            meters.clear()
+            other.clear()
+            tag.clear()
+            price.clear()
+            updateTime.clear()
+            seeNum.clear()
+            result = ""
+        num = 0
